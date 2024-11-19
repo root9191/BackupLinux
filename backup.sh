0dc072a3b9e3eb8a00b29492a1d98a679f247570
root9191 <idevacc@icloud.com>
2024-11-20 00:04:00 +0100

    10

--- backup.sh
+++ backup.sh
@@ -20,7 +20,7 @@ BACKUP_SSH="no"           # "yes" oder "no"
 BACKUP_SSH_KEYS="no"      # "yes" oder "no" - Private Schlüssel nur wenn ENCRYPT="true"
 
 # --- Basis-Verzeichnisse ---
-BASE_BACKUP_DIR="/mnt/backup"
+BASE_BACKUP_DIR="/mnt/Daten/lichti"
 TEMP_BASE_DIR="/var/tmp/bkp"
 
 # --- Logging Konfiguration ---
